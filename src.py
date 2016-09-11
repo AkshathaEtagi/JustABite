958c0c9882e667a1ed41620f10ce2b4b48f3d022
mamtaharris <mamtamariamharris@gmail.com>
2016-09-11 06:54:00 +0530

    Linked with input front end

--- src.py
+++ src.py
@@ -7,25 +7,28 @@ count = 0
 
 client = MongoClient('localhost', 27017)
 
-db = client.collection								#using database (collection)
-coll = db.foods										#using collection in the db (foods)
-curr = db.inputc
+db = client.testdb								#using database (collection)
+coll = db.foods									#using collection in the db (foods)
+curru= db.current
+curr = db.restveg
 curr_user = curr.find_one({})
+c= curru.find_one({})
+
 db.outputc.remove({})
 
-for prev in coll.find({"user_name":curr_user["user_name"]}):
+for prev in coll.find({"user_name":c["user_name"]}):
 	for other in coll.find():
 		if prev["user_name"] != other["user_name"] and prev["rest_name"]==other["rest_name"] and prev["dish_name"]==other["dish_name"]:
 			if prev["review"][1]==other["review"][1]:
 				x = abs(prev["review"][0] - other["review"][0])
 				y = abs(prev["review"][2] - other["review"][2])
 				res_sum = x + y
-		if res_sum < 3:
-			match = other
-			match_flag = 1
-			break
-	if match_flag  == 1:
+	if res_sum < 3:
+		match = other
+		match_flag = 1
 		break
+		if match_flag  == 1:
+			break
 
 if match_flag == 1:	#if a match is found with someone
 	if curr_user["veg"] == True:
